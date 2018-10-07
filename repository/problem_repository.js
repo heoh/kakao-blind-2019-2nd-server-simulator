330bae5ed2cccc98b06a0be79cb467d1b5e7eaf2
Heo-H <h0h6h2h5@gmail.com>
2018-10-08 00:34:03 +0900

    Implement ProblemRepository temporary

--- repository/problem_repository.js
+++ repository/problem_repository.js
@@ -1,6 +1,11 @@
 class ProblemRepository {
+    constructor() {
+        this._problems = {};
+        this._problems[0] = require(__dirname + '/../problems/problem_0.json');
+    }
+
     hasProblemId(problem_id) {
-        return false;
+        return this._problems.hasOwnProperty(problem_id);
     }
 }
 
