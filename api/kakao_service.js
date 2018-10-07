f99290810500602740d1062c11ff4bf9e338a815
Heo-H <h0h6h2h5@gmail.com>
2018-10-07 22:44:11 +0900

    Refactor for class coding convention

--- api/kakao_service.js
+++ api/kakao_service.js
@@ -1,23 +1,24 @@
 function KakaoService() {
-    const response = function (status, body) {
-        const res = {
-            'status': status,
-            'body': body
-        };
-        return res;
-    }
+}
 
-    this.start = function (user_key, problem_id, num_of_elevators) {
-        return response(200, null);
-    }
+KakaoService.prototype.start = function (user_key, problem_id, num_of_elevators) {
+    return response(200, null);
+}
 
-    this.onCalls = function (token) {
-        return response(200, null);
-    }
+KakaoService.prototype.onCalls = function (token) {
+    return response(200, null);
+}
 
-    this.action = function (token, action) {
-        return response(200, null);
-    }
+KakaoService.prototype.action = function (token, action) {
+    return response(200, null);
+}
+
+const response = function (status, body) {
+    const res = {
+        'status': status,
+        'body': body
+    };
+    return res;
 }
 
 module.exports = KakaoService;
