beb5380ee69329e3143a5f351979a765672cd8d4
Heo-H <h0h6h2h5@gmail.com>
2018-10-08 00:25:35 +0900

    Implement StateRepository class

--- repository/state_repository.js
+++ repository/state_repository.js
@@ -1,22 +1,51 @@
 class StateRepository {
+    constructor() {
+        this._tokens = {};
+        this._states = {};
+    }
+
     hasUserKey(user_key) {
-        return false;
+        return this._tokens.hasOwnProperty(user_key);
     }
     
     getByUserKey(user_key) {
-        return null;
+        const token = this._tokens[user_key];
+        return this.getByToken(token);
     }
     
     hasToken(token) {
-        return false;
+        return this._states.hasOwnProperty(token);
+    }
+
+    getByToken(token) {
+        return this._states[token];
     }
     
     add(state) {
-        return false;
+        const user_key = state.user_key;
+        const token = state.token;
+
+        if (this.hasUserKey(user_key)) return false;
+        if (this.hasToken(token)) return false;
+
+        this._tokens[user_key] = token;
+        this._states[token] = state;
+
+        return true;
     }
     
     remove(state) {
-        return false;
+        const user_key = state.user_key;
+        const token = state.token;
+
+        if (!this.hasUserKey(user_key) && !this.hasToken(token)) {
+            return false;
+        }
+
+        delete this._tokens[user_key];
+        delete this._states[token];
+
+        return true;
     }
 }
 
