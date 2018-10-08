aacc8bbb604e2911e2e6ffdaf12728d835c04498
Heo-H <h0h6h2h5@gmail.com>
2018-10-08 09:40:59 +0900

    Hide private field for json conversion

--- model/elevator.js
+++ model/elevator.js
@@ -1,3 +1,6 @@
+const _max_floor = new WeakMap();
+const _max_passengers = new WeakMap();
+
 class Elevator {
     constructor(id, max_passengers, max_floor) {
         this.id = id;
@@ -7,6 +10,11 @@ class Elevator {
         this._max_floor = max_floor;
         this._max_passengers = max_passengers;
     }
+
+    get _max_floor() { return _max_floor.get(this); }
+    set _max_floor(value) { return _max_floor.set(this, value); }
+    get _max_passengers() { return _max_passengers.get(this); }
+    set _max_passengers(value) { return _max_passengers.set(this, value); }
 }
 
 module.exports = Elevator;
