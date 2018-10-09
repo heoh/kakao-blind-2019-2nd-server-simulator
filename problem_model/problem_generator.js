b02dd1e55217551b94ed181fff99e988ce1fdeba
Heo-H <h0h6h2h5@gmail.com>
2018-10-09 15:40:50 +0900

    Implement constructor and setters

--- problem_model/problem_generator.js
+++ problem_model/problem_generator.js
@@ -1,30 +1,40 @@
 class ProblemGenerator {
-    setProblemId(id) {
+    constructor() {
+        this._problem_id = 0;
+        this._max_floor = 0;
+        this._max_passengers = 0;
+        this._total_calls = 0;
+        this._traffic_generators = [];
+        this._call_batch_size = {};
+        this._call_batch_interval = {};
+    }
 
+    setProblemId(id) {
+        this._problem_id = id;
     }
 
     setMaxFloor(floor) {
-
+        this._max_floor = floor;
     }
 
     setMaxPassengers(n) {
-
+        this._max_passengers = n;
     }
 
     setTotalCalls(n) {
-
+        this._total_calls = n;
     }
 
     addTrafficGenerator(traffic_gen, weight) {
-
+        this._traffic_generators.push({ 'traffic_gen': traffic_gen, 'weight': weight });
     }
 
     setCallBatchSize(flexible_int={ min, max, mean, variance }) {
-
+        this._call_batch_size = flexible_int;
     }
 
     setCallBatchInterval(flexible_int={ min, max, mean, variance }) {
-
+        this._call_batch_interval = flexible_int;
     }
 
     generate() {
