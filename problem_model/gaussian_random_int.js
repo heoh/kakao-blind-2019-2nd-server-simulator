d42a77c6cced4506afd78168e6526ad4beaef3be
Heo-H <h0h6h2h5@gmail.com>
2018-10-09 17:29:34 +0900

    Support case(variance = 0)

--- problem_model/gaussian_random_int.js
+++ problem_model/gaussian_random_int.js
@@ -1,24 +1,53 @@
 const gaussian = require('gaussian');
 
 class GaussianRandomInt {
-    constructor(flexible_int={ min, max, mean, variance }) {
-        this.min = flexible_int.min;
-        this.max = flexible_int.max;
-        this._distribution = gaussian(mean, variance);
+    constructor({ min, max, mean, variance }) {
+        this.min = min;
+        this.max = max;
+        this._mean = mean;
+        this._variance = variance;
+        this._distribution = null;
+        this._updateDistribution();
     }
 
-    get mean() { return this._distribution.mean; }
-    set mean(value) { this._distribution.mean = value; }
-    get variance() { return this._distribution.variance; }
-    set variance(value) { this._distribution.variance = value; }
+    get mean() {
+        return this._mean;
+    }
+
+    set mean(value) {
+        this._variance = value;
+        this._updateDistribution();
+    }
+
+    get variance() {
+        return this._variance;
+    }
+
+    set variance(value) {
+        this._variance = value;
+        this._updateDistribution();
+    }
 
     get() {
+        if (this.variance == 0) {
+            return this.mean;
+        }
+
         while (true) {
             const gaussianRandom = this._distribution.ppf(Math.random());
             const randomInt = Math.round(gaussianRandom);
             if ((randomInt >= this.min) && (randomInt <= this.max)) {
                 return randomInt;
             }
+        }
+    }
+
+    _updateDistribution() {
+        if (this._variance != 0) {
+            this._distribution = gaussian(this._mean, this._variance);
+        }
+        else {
+            this._distribution = null;
         }
     }
 }
