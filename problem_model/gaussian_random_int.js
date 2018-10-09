b0fc2d04afc553a7b840460596d9135382e3f7cf
Heo-H <h0h6h2h5@gmail.com>
2018-10-09 16:32:30 +0900

    Implement GaussianRandomInt

--- problem_model/gaussian_random_int.js
+++ problem_model/gaussian_random_int.js
@@ -1,13 +1,25 @@
+const gaussian = require('gaussian');
+
 class GaussianRandomInt {
     constructor(flexible_int={ min, max, mean, variance }) {
         this.min = flexible_int.min;
         this.max = flexible_int.max;
-        this.mean = flexible_int.mean;
-        this.variance = flexible_int.variance;
+        this._distribution = gaussian(mean, variance);
     }
 
+    get mean() { return this._distribution.mean; }
+    set mean(value) { this._distribution.mean = value; }
+    get variance() { return this._distribution.variance; }
+    set variance(value) { this._distribution.variance = value; }
+
     get() {
-        return 0;
+        while (true) {
+            const gaussianRandom = this._distribution.ppf(Math.random());
+            const randomInt = Math.round(gaussianRandom);
+            if ((randomInt >= this.min) && (randomInt <= this.max)) {
+                return randomInt;
+            }
+        }
     }
 }
 
